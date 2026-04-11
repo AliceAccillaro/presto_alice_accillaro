182641f19146e9f51abfe56c1464ad5adf762e17
AliceAccillaro <aaccillaro@gmail.com>
2026-04-11 17:03:15 +0200

    end of user story 9

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -5,7 +5,9 @@ namespace App\Jobs;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Spatie\Image\Enums\AlignPosition;
+use Spatie\Image\Enums\Fit;
 use Spatie\Image\Enums\ImageDriver as SpatieImageDriver;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Image;
 
 class ResizeImage implements ShouldQueue
@@ -29,6 +31,7 @@ class ResizeImage implements ShouldQueue
             ? "crop_{$this->w}x{$this->h}_"
             : "crop_{$this->w}_";
         $destPath = storage_path('app/public/' . $this->path . '/' . $prefix . $this->fileName);
+        $watermarkPath = public_path('images/watermark-presto.svg');
 
         $image = Image::useImageDriver(SpatieImageDriver::Imagick)->loadFile($srcPath);
 
@@ -39,6 +42,22 @@ class ResizeImage implements ShouldQueue
                 ->resizeCanvas($this->w, $this->h, AlignPosition::Center, false, '#ffffff');
         } else {
             $image->width($this->w);
+        }
+
+        if (file_exists($watermarkPath)) {
+            $image->watermark(
+                $watermarkPath,
+                AlignPosition::TopLeft,
+                1,
+                1,
+                Unit::Percent,
+                40,
+                Unit::Percent,
+                0,
+                Unit::Pixel,
+                Fit::Contain,
+                100
+            );
         }
 
         $image->save($destPath);
